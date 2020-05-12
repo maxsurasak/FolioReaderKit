a25abfa6363bdb31af8f7963c79ec798b54b540b
pair <t.nitikarn@gmail.com>
2020-05-12 11:54:38 +0700

    fix webkit

--- Source/FolioReaderCenter.swift
+++ Source/FolioReaderCenter.swift
@@ -5,7 +5,7 @@
 //  Created by Heberti Almeida on 08/04/15.
 //  Copyright (c) 2015 Folio Reader. All rights reserved.
 //
-
+import WebKit
 import UIKit
 import ZFDragableModalTransition
 
@@ -1289,7 +1289,7 @@ open class FolioReaderCenter: UIViewController, UICollectionViewDelegate, UIColl
         // Perform the page after a short delay as the collection view hasn't completed it's transition if this method is called (the index paths aren't right during fast scrolls).
         delay(0.2, closure: { [weak self] in
             if (self?.readerConfig.scrollDirection == .horizontalWithVerticalContent),
-                let cell = ((scrollView.superview as? UIWebView)?.delegate as? FolioReaderPage) {
+                let cell = ((scrollView.superview as? WKWebView)?.navigationDelegate as? FolioReaderPage) {
                 let currentIndexPathRow = cell.pageNumber - 1
                 self?.currentWebViewScrollPositions[currentIndexPathRow] = scrollView.contentOffset
             }
